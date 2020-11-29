20eddc903a0f763bfb39dbd27349b40ee80c8eeb
hrntsm <contact@hrntsm.com>
2020-11-29 19:21:27 +0900

    Update

--- GrasshopperCISample/CISampleComponent.cs
+++ GrasshopperCISample/CISampleComponent.cs
@@ -56,7 +56,7 @@ namespace GrasshopperCISample
             if (!DA.GetData(0, ref a)) { return; }
             if (!DA.GetData(1, ref b)) { return; }
 
-            sum = CIMath.Add(a, b);
+            double sum = Util.Add1(a, b);
 
             DA.SetData(0, sum);
         }
