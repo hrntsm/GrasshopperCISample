0fddb2d2f2f381a6b486afd10179f5073c6f1280
hrntsm <contact@hrntsm.com>
2020-12-11 12:20:08 +0900

    Add Test

---
+++ GrasshopperCISampleTests/UtilTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrasshopperCISample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrasshopperCISample.Tests
+{
+    [TestClass()]
+    public class UtilTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
